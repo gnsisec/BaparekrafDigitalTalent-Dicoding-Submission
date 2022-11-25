750084578667cd932267ef3e45f6d73b83e8b0e5
mattivityroom <mattivityroom@gmail.com>
2022-11-26 06:14:35 +0700

    About page finished, but splash screen was delayed

---
+++ app/src/main/java/com/example/pokedexscarlet/AboutPage.kt
@@ -0,0 +1,22 @@
+package com.example.pokedexscarlet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class AboutPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_page)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+        android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+}
--- app/src/main/java/com/example/pokedexscarlet/MainActivity.kt
+++ app/src/main/java/com/example/pokedexscarlet/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.pokedexscarlet
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedexscarlet.R.id.rv_pokemons
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private var list_pokemon: ArrayList<Pokemon> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -19,7 +25,24 @@ class MainActivity : AppCompatActivity() {
         rvPokemon.setHasFixedSize(true)
 
         list_pokemon.addAll(PokemonData.listPokemon)
+        installSplashScreen()
         showPokemonList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.about_me, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.about_page -> {
+                val aboutPageLink = Intent(this@MainActivity, AboutPage::class.java)
+                startActivity(aboutPageLink)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showPokemonList() {
